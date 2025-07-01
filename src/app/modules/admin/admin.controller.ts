26e7a063be07cede83f1c69e280ad98bd831f571
Mehedi Hasan <thecodermehedi@gmail.com>
2025-07-01 09:01:06 +0600

    fixed formatting

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -4,32 +4,32 @@ import { admin_query_fields } from "./admin.constant";
 import { AdminServices } from "./admin.service";
 
 const getAdmins: RequestHandler = async (req, res) => {
-  try {
-    const filters = pick(req.query, admin_query_fields);
-    const options = pick(req.query, ['limit', 'page', 'sort', 'order'])
-    const result = await AdminServices.getAdmins(filters, options);
-    if (result.length === 0) {
-      res.status(200).json({
-        status: "success",
-        message: "No administrators found.",
-        data: [],
-      });
-    }
-    res.status(200).json({
-      status: "success",
-      message: "Administrators retrieved successfully!",
-      data: result,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "error",
-      message: error?.name || "Something went wrong",
-      error,
-    });
-    return;
-  }
+	try {
+		const filters = pick(req.query, admin_query_fields);
+		const options = pick(req.query, ["limit", "page", "sort", "order"]);
+		const result = await AdminServices.getAdmins(filters, options);
+		if (result.length === 0) {
+			res.status(200).json({
+				status: "success",
+				message: "No administrators found.",
+				data: [],
+			});
+		}
+		res.status(200).json({
+			status: "success",
+			message: "Administrators retrieved successfully!",
+			data: result,
+		});
+	} catch (error) {
+		res.status(500).json({
+			status: "error",
+			message: error?.name || "Something went wrong",
+			error,
+		});
+		return;
+	}
 };
 
 export const AdminControllers = {
-  getAdmins,
+	getAdmins,
 };
--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -3,53 +3,59 @@ import { admin_search_fields } from "./admin.constant";
 
 const prisma = new PrismaClient();
 
-
-const calculate_pagination = (options: { page?: number, limit?: number, sort?: string, order?: string }) => {
-  const page: number = Number(options.limit) && Number(options.page) ? Number(options.page) : 1;
-  const limit: number = Number(options.limit) && Number(options.page) ? Number(options.limit) : 10;
-  const skip: number = (page - 1) * limit;
-  const sort = options.sort && options.order ? options.sort : 'createdAt';
-  const order = options.sort && options.order ? options.order : 'desc'
-  return { page, limit, skip, sort, order }
-}
+const calculate_pagination = (options: {
+	page?: number;
+	limit?: number;
+	sort?: string;
+	order?: string;
+}) => {
+	const page: number =
+		Number(options.limit) && Number(options.page) ? Number(options.page) : 1;
+	const limit: number =
+		Number(options.limit) && Number(options.page) ? Number(options.limit) : 10;
+	const skip: number = (page - 1) * limit;
+	const sort = options.sort && options.order ? options.sort : "createdAt";
+	const order = options.sort && options.order ? options.order : "desc";
+	return { page, limit, skip, sort, order };
+};
 
 const getAdmins = async (params: any, options: any) => {
-  const { search, ...filters } = params;
-  const { page, limit, skip, sort, order } = calculate_pagination(options)
-  const AndConditions: Prisma.AdminWhereInput[] = [];
-  if (search) {
-    AndConditions.push({
-      OR: admin_search_fields.map((field) => ({
-        [field]: {
-          contains: search,
-          mode: "insensitive",
-        },
-      })),
-    });
-  }
-  if (Object.keys(filters).length > 0) {
-    AndConditions.push({
-      AND: Object.keys(filters).map((key) => ({
-        [key]: {
-          equals: filters[key],
-        },
-      })),
-    });
-  }
-  const WhereConditions = { AND: AndConditions };
-  const result = await prisma.admin.findMany({
-    where: WhereConditions,
-    skip,
-    take: limit,
-    orderBy: {
-      [sort]: order
-    }
-  });
-  return result;
+	const { search, ...filters } = params;
+	const { page, limit, skip, sort, order } = calculate_pagination(options);
+	const AndConditions: Prisma.AdminWhereInput[] = [];
+	if (search) {
+		AndConditions.push({
+			OR: admin_search_fields.map((field) => ({
+				[field]: {
+					contains: search,
+					mode: "insensitive",
+				},
+			})),
+		});
+	}
+	if (Object.keys(filters).length > 0) {
+		AndConditions.push({
+			AND: Object.keys(filters).map((key) => ({
+				[key]: {
+					equals: filters[key],
+				},
+			})),
+		});
+	}
+	const WhereConditions = { AND: AndConditions };
+	const result = await prisma.admin.findMany({
+		where: WhereConditions,
+		skip,
+		take: limit,
+		orderBy: {
+			[sort]: order,
+		},
+	});
+	return result;
 };
 
 export const AdminServices = {
-  getAdmins,
+	getAdmins,
 };
 
 /*
