89c40fcb54a4262dd8e2fb0afc123913ec41683c
Mehedi Hasan <thecodermehedi@gmail.com>
2025-07-12 01:57:12 +0600

    fixed formatting

--- src/middlewares/authValidator.ts
+++ src/middlewares/authValidator.ts
@@ -6,22 +6,22 @@ import type { JwtUserPayload } from "@/modules/auth/auth.types.js";
 import httpStatus from "@/shared/httpStatus.js";
 
 const authValidator = (...roles: string[]): RequestHandler => {
-  return async (req, res, next) => {
-    try {
-      const accessToken = req.headers.authorization;
-      if (!accessToken) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!");
-      }
-      const decoded = jwtHelpers.verifyToken(accessToken, config.accessTokenSecret) as JwtUserPayload;
-      req.user = decoded;
-      if (roles.length && !roles.includes(decoded.role)) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!");
-      }
-      next();
-    } catch (error) {
-      next(error);
-    }
-  };
+	return async (req, _res, next) => {
+		try {
+			const accessToken = req.headers.authorization;
+			if (!accessToken) {
+				throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+			}
+			const decoded = jwtHelpers.verifyToken(accessToken, config.accessTokenSecret) as JwtUserPayload;
+			req.user = decoded;
+			if (roles.length && !roles.includes(decoded.role)) {
+				throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+			}
+			next();
+		} catch (error) {
+			next(error);
+		}
+	};
 };
 
 export default authValidator;
